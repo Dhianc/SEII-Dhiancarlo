30189e24f6f80906fdb8161ac4e0e1506e908de2
Dhianc <dc_1094@hotmail.com>
2021-03-13 20:04:44 -0300

    Dictionaries

--- Semana02/prog04.py
+++ Semana02/prog04.py
@@ -80,3 +80,6 @@ art_courses = {'History', 'Math', 'Art', 'Design'}
 print(cs_courses.intersection(art_courses))
 print(cs_courses.difference(art_courses))
 print(cs_courses.union(art_courses))
+
+empty_list = []
+empty_list = list() #lista vazia
